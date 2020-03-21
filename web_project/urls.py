a919254712a22052d96ab0bce62a15937c6bdab2
juliethjar <60681572+juliethjar@users.noreply.github.com>
2020-03-21 08:00:57 -0700

    static images fix

--- web_project/urls.py
+++ web_project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 from bearbites import views
+from django.conf.urls.static import serve, static
+from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('',include('bearbites.urls')),
-]
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+] 
